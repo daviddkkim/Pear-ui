c17266501f6f83904bdab3c6c407acf1c4655b9d
David Kim <david.kim@richmond.edu>
2022-05-05 14:21:04 -0400

    add buttoon size type and improve buttons

--- packages/hazy-core/src/Button.tsx
+++ packages/hazy-core/src/Button.tsx
@@ -1,11 +1,24 @@
 import * as React from "react";
+import { ButtonSize } from "./types";
 
 export interface ButtonProps {
   children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
+  style?: React.CSSProperties;
+  size?: ButtonSize;
 }
 
 export function Button(props: ButtonProps) {
-  return <button>{props.children}</button>;
+  return (
+    <button
+      onClick={props.onClick}
+      disabled={props.disabled}
+      style={props.style}
+    >
+      {props.children}
+    </button>
+  );
 }
 
 Button.displayName = "Button";
---
+++ packages/hazy-core/src/types.ts
@@ -0,0 +1,6 @@
+export enum ButtonSize {
+  ExtraSmall = "xs",
+  Small = "sm",
+  Medium = "md",
+  Large = "lg",
+}
