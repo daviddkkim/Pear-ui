645452425d5948e8e3a79ac70d227cecdc420bd0
David Kim <david.kim@richmond.edu>
2022-05-04 22:47:26 -0400

    add react view

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@hazy/core";
 import { useIsomorphicLayoutEffect } from "@hazy/utils";
+import { View, PropTypes } from "react-view";
 
 export default function Docs() {
   useIsomorphicLayoutEffect(() => {
@@ -8,7 +9,34 @@ export default function Docs() {
   return (
     <div>
       <h1>hazy Documentation</h1>
-      <Button>Click me</Button>
+      <View
+        componentName="Button"
+        props={{
+          children: {
+            value: "Hello",
+            type: PropTypes.ReactNode,
+            description: "Visible label.",
+          },
+          onClick: {
+            value: '() => alert("click")',
+            type: PropTypes.Function,
+            description: "Function called when button is clicked.",
+          },
+          disabled: {
+            value: false,
+            type: PropTypes.Boolean,
+            description: "Indicates that the button is disabled",
+          },
+        }}
+        scope={{
+          Button,
+        }}
+        imports={{
+          "your-button-component": {
+            named: ["Button"],
+          },
+        }}
+      />
     </div>
   );
 }
